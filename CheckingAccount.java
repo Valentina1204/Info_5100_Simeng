69bebb28dd35cd3572d9112e71f5fc8270767a34
Valentina1204 <59623564+Valentina1204@users.noreply.github.com>
2020-02-28 10:15:32 -0800

    Add files via upload

---
+++ CheckingAccount.java
@@ -0,0 +1,50 @@
+public class CheckingAccount extends Account
+{
+    static private double MinBalance = 100;
+    static private double Fee = 10;
+
+    // Constructor
+    public CheckingAccount(String fname, String lname, double cb)
+    {
+        super(fname, lname, cb);
+    }
+
+    @Override
+    public double DebitTransaction(double debitAmount)
+    {
+        super.DebitTransaction(debitAmount);
+        ChargeFee();
+        return CurBalance;
+    }
+
+    @Override
+    public double CreditTransaction(double creditAmount)
+    {
+        super.CreditTransaction(creditAmount);
+        ChargeFee();
+        return CurBalance;
+    }
+
+    private void ChargeFee()
+    {
+        if (CurBalance < MinBalance)
+        {
+            CurBalance -= Fee;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        CheckingAccount ch1 = new CheckingAccount("Steve", "Jobs", 50);
+        System.out.println(ch1);
+
+        ch1.DebitTransaction(0.25);
+        System.out.println(ch1);
+
+        ch1.CreditTransaction(8.00);
+        System.out.println(ch1);
+
+        ch1.CreditTransaction(500);
+        System.out.println(ch1);
+    }
+}
