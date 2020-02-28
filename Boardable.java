34a31be9f6a5a1515a9cdd64ecb3cca5e1515fac
Valentina1204 <59623564+Valentina1204@users.noreply.github.com>
2020-02-28 10:16:18 -0800

    Delete Boardable.java

--- Boardable.java
+++
@@ -1,6 +0,0 @@
-interface Boardable
-{
-    public void setBoardStart(int month, int day, int year);
-    public void setBoardEnd(int month, int day, int year);
-    public boolean boarding(int month, int day, int year);
-}
